a1d8d8fb8d91ce747bfb402b84bb3320839b2e47
Peter Boone <boonepeterg@gmail.com>
2019-03-01 09:39:07 -0500

    change names

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ from shutil import rmtree
 from setuptools import find_packages, setup, Command
 
 # Package meta-data.
-NAME = 'elexio-api'
+NAME = 'elexio_api'
 DESCRIPTION = "A small script to pull info from elexio's api"
 URL = 'https://github.com/boonepeter/elexio-api'
 EMAIL = 'boonepeter@gmail.com'
